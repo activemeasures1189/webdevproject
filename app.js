1fd8cb83b4a863cb60217d5a9f580cf7ccaffb62
techsabby <ksabby89@yahoo.com>
2021-02-09 13:18:34 +0800

    second db url

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelpcamp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology:true,
     useFindAndModify:true
 
 }).then(()=>{
