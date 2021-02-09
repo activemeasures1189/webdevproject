79e20d17b959a5515fea1508a33c7dad2cb42834
techsabby <ksabby89@yahoo.com>
2021-02-09 13:04:24 +0800

    db url fix

--- app.js
+++ app.js
@@ -48,8 +48,10 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 
+// DATABASE URL
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelpcamp';
 // DATABASE CONNECT
-mongoose.connect('mongodb://localhost:27017/yelpcamp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology:true,
